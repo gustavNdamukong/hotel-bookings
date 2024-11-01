c0adece63daa9e5338947e6b5384a5584fa33cbd
Gustav Ndamukong <gustavfn@yahoo.co.uk>
2024-10-31 23:38:59 -0400

    Fixed base layout footer styling

--- static/js/app.js
+++ static/js/app.js
@@ -117,7 +117,7 @@ function BookARoom(roomID, token) {
       <form id="check-availability-form" action="" method="post" novalidate class="needs-validation">
           <div class="form-row">
               <div class="col">
-                  <div class="form-row" id="reservation-dates-modal">
+                  <div class="row form-row" id="reservation-dates-modal">
                       <div class="col">
                           <input disabled required class="form-control" type="text" name="start" id="start" placeholder="Arrival">
                       </div>
